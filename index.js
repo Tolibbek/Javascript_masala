a82e4ffacfadfb0109f7317d960e2c9cf7f7c681
Tolibbek <tolibbekalisherov876@gmail.com>
2022-10-16 10:16:44 +0500

    f

---
+++ index.js
@@ -0,0 +1,42 @@
+// let obj = {
+//     name: "Tolibbek",
+//     greet: function (a, b) {
+//         console.log(a + " " + b)
+//     }
+// }
+
+// let a = prompt("Mening ismim", "Mening ismim")
+// let b = prompt("Tolibbek", "Tolibbek")
+// obj.greet(a,b)
+
+
+// console.log("Mening ismim " , obj.name);
+// // console.log(obj.name)
+
+/*
+let person = {
+    name: "Tolibbek",
+    age: 14 ,
+    gender: "kotta bola"
+}
+console.log("mening ismim " , person.name , "Men " , person.name , person.gender , "man");
+*/
+
+
+// let javascript_tests = 1000
+// try{
+//     javascript_tests = 550
+// } finally {
+//     javascript_tests = 54
+// }
+// console.log(javascript_tests);
+//  let x = "shirin"
+// x= 'foydali'
+// print("olma" + x);
+// var x = "shirin"
+//  function( ) :
+//  global x
+//  x = 'foydali'
+//  print("olma" + x)
+//  function()
+//  print("olma" + x);
